98e78bbe50497c6bb4c6e7901ee22f8b572f9ecc
Kenny Elliott4 <kenny@neoserve.com>
2025-09-07 13:55:42 -0500

    update compose

--- docker/php-fpm/entrypoint.sh
+++ docker/php-fpm/entrypoint.sh
@@ -9,7 +9,7 @@ set -e
 if [ ! "$(ls -A /var/www/storage)" ]; then
   echo "Initializing storage directory..."
   cp -R /var/www/storage-init/. /var/www/storage
-  chown -R www-data:www-data /var/www/storage
+  chown -R appuser:appuser /var/www/storage
 fi
 
 # Remove storage-init directory
